8a03f3237efe97b2081b59c72d08a8fde60b6638
JojoDuke <jojoamankwa@gmail.com>
2022-10-08 14:02:20 +0000

    getting data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import Axios from 'axios';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("localhost:3001/getUsers")
+      .then((res) => {
+        setUsersList(res.data)
+      })
+      .catch((error) => console.log(error))
+  }, []);
+
   return (
     <div className="App">
-      hi
+      <div className='userDisplay'>
+        {
+          usersList.map((user) => {
+            return (
+              <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
