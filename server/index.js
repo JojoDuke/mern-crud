2432af27249307b0ce36e0f0fec89ec5c446a325
JojoDuke <jojoamankwa@gmail.com>
2022-10-08 13:00:49 +0000

    sure

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const UserModel = require("./Models/Users");
 
 const port = 3001;
 
@@ -9,8 +10,14 @@ const port = 3001;
 mongoose.connect("mongodb+srv://User0:123@cluster0.etmxs8f.mongodb.net/mern-crud?retryWrites=true&w=majority");
 
 //
-app.get("getUsers", (req, res) => {
-
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if(err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 
