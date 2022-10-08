4a06a10b3a636b63b1130b85c56f3f27de59c93e
JojoDuke <jojoamankwa@gmail.com>
2022-10-08 00:56:56 +0000

    more

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const port = 3001;
+
+//MongoDB connection server
+mongoose.connect("mongodb+srv://User0:123@cluster0.etmxs8f.mongodb.net/mern-crud?retryWrites=true&w=majority");
+
+//
+app.get("getUsers", (req, res) => {
+
+});
+
 
 // This starts the server on the given port
 app.listen(port, () => {
